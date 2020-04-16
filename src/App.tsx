25d3177366c23502928338aabfe934da74426eb5
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-04-17 00:09:39 +0900

    Create recording video

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [recording, setRecording] = React.useState(false);
+  React.useEffect(() => {
+    console.warn(recording);
+    if (recording) {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: { width: window.innerWidth, height: window.innerHeight },
+          audio: true,
+        })
+        .then((stream) => {
+          document.querySelector('video')!.srcObject = stream;
+        });
+    } else {
+      const localStream = document.querySelector('video')!.srcObject;
+      if (localStream) {
+        const mediaStream = localStream as MediaStream;
+        const tracks = mediaStream.getTracks();
+        tracks.forEach((track) => {
+          track.stop();
+        });
+      }
+    }
+  }, [recording]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="video" autoPlay className="videoObject"></video>
+      <div className="recordButton" onClick={() => setRecording(!recording)}>
+        Record
+      </div>
     </div>
   );
-}
+};
 
 export default App;
