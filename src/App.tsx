baa381a71637c141677a61f311db4d0a73913da8
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-04-17 00:44:26 +0900

    Create webrtc

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,75 @@
 import React from 'react';
 import './App.css';
 
+const startPeer = async (
+  pc: RTCPeerConnection,
+  stream: MediaStream,
+  document: Document
+) => {
+  pc.ontrack = (ev) => {
+    console.warn(ev.streams[0]);
+    const remoteVideo = document.getElementById(
+      'remoteVideo'
+    ) as HTMLVideoElement;
+    remoteVideo.srcObject = ev.streams[0];
+  };
+  const localVideo = document.getElementById('localVideo') as HTMLVideoElement;
+  localVideo!.srcObject = stream;
+  for (const track of stream.getTracks()) {
+    pc.addTrack(track, stream);
+  }
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  await pc.setRemoteDescription(offer);
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+
+  console.warn(pc.localDescription);
+  console.warn(pc.remoteDescription);
+};
+
+const stopPeer = (document: Document) => {
+  const localStream = document.querySelector('video')!.srcObject;
+  if (localStream) {
+    const mediaStream = localStream as MediaStream;
+    const tracks = mediaStream.getTracks();
+    tracks.forEach((track) => {
+      track.stop();
+    });
+  }
+};
+
 const App: React.FC = () => {
   const [recording, setRecording] = React.useState(false);
+  const [pc] = React.useState(
+    new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:1.2.3.4' }],
+    })
+  );
+
   React.useEffect(() => {
-    console.warn(recording);
     if (recording) {
       navigator.mediaDevices
         .getUserMedia({
           video: { width: window.innerWidth, height: window.innerHeight },
           audio: true,
         })
-        .then((stream) => {
-          document.querySelector('video')!.srcObject = stream;
+        .then(async (stream) => {
+          startPeer(pc, stream, document);
         });
+      pc.addEventListener('icecandidate', (ev) => {
+        if (ev.candidate !== null) {
+        }
+      });
     } else {
-      const localStream = document.querySelector('video')!.srcObject;
-      if (localStream) {
-        const mediaStream = localStream as MediaStream;
-        const tracks = mediaStream.getTracks();
-        tracks.forEach((track) => {
-          track.stop();
-        });
-      }
+      stopPeer(document);
     }
-  }, [recording]);
+  }, [pc, recording]);
+
   return (
     <div className="App">
-      <video id="video" autoPlay className="videoObject"></video>
+      <video autoPlay className="videoObject" id="localVideo"></video>
+      <video autoPlay className="videoObject" id="remoteVideo"></video>
       <div className="recordButton" onClick={() => setRecording(!recording)}>
         Record
       </div>
